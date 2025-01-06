679c319a5c0dbddda7fb5f26586784f49bee8779
NikGlad <90272508+NikGlad@users.noreply.github.com>
2025-01-06 11:31:01 +0400

    06_01

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.util.*;
+
+//import static com.sun.org.apache.bcel.internal.classfile.Utility.printArray;
+import static java.util.Arrays.asList;
+
+class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
