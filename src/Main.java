97916cfd7bf3b7a198d8f2212b93f416b48ff634
NikGlad <90272508+NikGlad@users.noreply.github.com>
2025-01-06 12:38:55 +0400

    06_01_3

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,20 @@ import java.util.*;
 //import static com.sun.org.apache.bcel.internal.classfile.Utility.printArray;
 import static java.util.Arrays.asList;
 
+//class Main{
+//    public static void main(String[] args) {
+//        for (int i = 0; i < 5; i++) {
+//            if (i == 3){
+//                System.out.println("Hello World");
+//            } else{
+//                System.out.println("no");
+//            }
+//
+//        }
+//
+//    }
+//}
+
 class Main{
-    public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            if (i == 3){
-                System.out.println("Hello World");
-            } else{
-                System.out.println("no");
-            }
 
-        }
-
-    }
 }
